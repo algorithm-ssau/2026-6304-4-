16e2f7211f165184b411d83749b01d0f37d41901
nikitosikoz <147699135+nikitosikoz@users.noreply.github.com>
2026-04-30 16:13:56 +0400

    Refactor FunPayApiGateway with properties and methods

--- src/gateaway/funpay_api.py
+++ src/gateaway/funpay_api.py
@@ -4,18 +4,30 @@ from FunPayAPI import Account, Runner, types, enums
 @dataclass
 class FunPayApiGateaway:
    golden_token: str
+   username: str
 
    _account: Account | None = None
+   _runner: Runner | None = None
 
-   def get_account(self):
+   @property
+   def account(self):
       if not self._account:
          self._account = Account(self.golden_token).get()
-
       return self._account
 
-   async def send_message(self, text):
-      acc = self.get_account()
-      # acc.send_message()
+   @property
+   def runner(self):
+      if not self._runner:
+         acc = self.account
+         self._runner = Runner(acc)
+      
+      return self._runner
 
-   async def l(self):
-      acc = self.get_account()
+   async def send_message(self, chat_id, text):
+      acc = self.account
+      acc.send_message(chat_id, text)
+
+   def get_listener(self):
+      runner = self.runner
+      for event in runner.listen(requests_delay=4):
+         yield event
