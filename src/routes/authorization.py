71c286ef01715dd85b45b88e084f721ea1954449
nedraky <95582214+ykarden@users.noreply.github.com>
2026-05-14 23:09:47 +0400

    Update authorization.py

--- src/routes/authorization.py
+++ src/routes/authorization.py
@@ -4,22 +4,22 @@ from schemas.authorization.auth_tg import AuthTg
 from services.user_service import UserService
 
 
-router = APIRouter(prefix="auth")
+router = APIRouter(prefix="/auth")
 
-@router.get("/refresh")
-async def refresh_token(
+# @router.get("/refresh")
+# async def refresh_token(
 
-):
-    pass
+# ):
+#     pass
 
-@router.post("/register")
-async def register(
-    body: AuthTg,
-    service: UserService = Depends(get_user_service)
-):
-    pass
+# @router.post("/register")
+# async def register(
+#     body: AuthTg,
+#     service: UserService = Depends(get_user_service)
+# ):
+#     pass
 
 
-@router.post("/login")
-async def login():
-    pass
+# @router.post("/login")
+# async def login():
+#     pass
